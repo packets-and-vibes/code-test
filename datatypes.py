82fa616becf02274a2b53da845a072f39ac81dd4
Kenny Hutto <kenny@devnet.guru>
2026-05-27 20:20:13 -0600

    Created data type py file

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+favorite_car = [
+    {
+        'make': 'Tesla',
+        'model': 'Model S',
+        'year': 2022, # Number (int)
+        'price': 79999.99, # Float
+        'features': ['Autopilot', 'Electric', 'All-Wheel Drive'], # List
+        'is_electric': True, #Boolean
+        'owner': None # None
+    },
+    {
+        'make': 'Porsche',
+        'model': '911 Carrera',
+        'year': 2023, # Number (int)
+        'price': 106500.00, #float
+        'features': ['Sport Mode', 'Rear-Wheel Drive', 'Turbocharged'], # List
+        'is_electric': False, #Boolean
+        'owner': None # None
+    }    
+]
