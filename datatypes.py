f2b36defcd0152390f13f2f3b39f01b99ffc2b17
Kenny Hutto <kenny@devnet.guru>
2026-05-27 20:47:47 -0600

    Added for loops and used any() function

--- datatypes.py
+++ datatypes.py
@@ -19,12 +19,32 @@ favorite_cars = [
     }    
 ]
 
-if favorite_cars == []:
-    print("This list is empty")
-    print('Please add some items to the list\nThank you')
-elif favorite_cars[1]['make'] == 'Porsche':
-    print('The second iem is a Porsche')
-    print(favorite_cars[1]['make'] + ' ' + favorite_cars[1]['model'])
+# if favorite_cars == []:
+#     print("This list is empty")
+#     print('Please add some items to the list\nThank you')
+# elif favorite_cars[1]['make'] == 'Porsche':
+#     print('The second iem is a Porsche')
+#     print(favorite_cars[1]['make'] + ' ' + favorite_cars[1]['model'])
+# else:
+#     print('This list is not empty')
+#     print(favorite_cars[1]['make'] + ' ' + favorite_cars[1]['model'])
+
+# print(favorite_cars[0])
+# print(favorite_cars[1]) # Each item requires a separate print statement
+
+# for car in favorite_cars: # Basic for loop
+#     print(car)
+#     print("Next item:")
+
+for car in favorite_cars:
+    if car['make'] == 'Porsche':
+        print("This car is a Porsche")
+        print(car['make']+ ' ' + car['model'])
+    else:
+        print('This car is not a Porsche')
+        print(car['make']+ ' ' + car['model'])
+
+if any(car['make'] == 'Porsche' for car in favorite_cars): # Using any function
+    print('Porsche found!')
 else:
-    print('This list is not empty')
-    print(favorite_cars[1]['make'] + ' ' + favorite_cars[1]['model'])
+    print('There is no Porsche in this list')
