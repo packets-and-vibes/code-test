1b747aaf96185f6da3424cf26fa6142cc03d4b2f
Kenny Hutto <kenny@devnet.guru>
2026-05-28 20:26:45 -0600

    Organized into folder modules

--- func_test.py
+++
@@ -1,4 +0,0 @@
-from funcs.my_module import cube_number, greet_person
-
-print(cube_number(3))
-print(greet_person('Kenny'))
---
+++ module_6_functions/func_test.py
@@ -0,0 +1,4 @@
+from module_6_functions.funcs.my_module import cube_number, greet_person
+
+print(cube_number(3))
+print(greet_person('Kenny'))
